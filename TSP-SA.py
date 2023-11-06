b736b88bf37557658c444307a5c92606bc959028
lucianoSF <lukfraga@hotmail.com>
2023-11-06 15:51:54 -0300

    Update results

--- TSP-SA.py
+++ TSP-SA.py
@@ -4,6 +4,7 @@ import time
 
 code = 'ar35'
 
+# Leitura dos dados, cidades e distâncias entre cada par
 def read_distances(file_name='cidades/' + code + '.json'):
     
     fp = open(file_name)
@@ -11,33 +12,34 @@ def read_distances(file_name='cidades/' + code + '.json'):
     return (distances['cities'], distances['distances'])
 
 
+#Criar uma solução inicial
 def create_solution(num_cities):
     rc = num_cities
     solution = []
     list_of_cities = list(range(1, rc+1))
     
+    # inserir cidades de forma aleatória na solução inicial
     while(rc != 1):
         random_city_index = random.randint(0, rc-1)
-        
+        # inserção de cidade na solução e remoção do conjunto de cidades disponíveis
         solution.append(list_of_cities[random_city_index])
         list_of_cities.remove(list_of_cities[random_city_index])
         rc = rc - 1
-    
+    # Inserir a última cidade, e única restante em list_of_cities
     solution.append(list_of_cities[0])
     return solution
     
+#Criar uma solução candidata a partir da solução inicial 
 def create_new_solution(initial_solution, num_cities):
-    
+    # Escolha de duas cidades na solução inicial para serem  alternadas
     cityX = random.randint(0, num_cities-1)
     cityY = cityX
-
+    # Garantir que são duas cidades diferentes
     while(cityX == cityY):
         cityY = random.randint(0, num_cities-1)
         
-        
-    #print(cityX, cityY)  
     new_solution = list(initial_solution)
-    
+    # Troca de posição entre as cidades
     aux = new_solution[cityX]
     new_solution[cityX] = new_solution[cityY]
     new_solution[cityY] = aux
@@ -46,7 +48,7 @@ def create_new_solution(initial_solution, num_cities):
 
     
     
-    
+# Calculo do custo de uma solução, ou seja a distância total a ser percorrida
 def calculate_cost(solution, distances):
     cost = 0
     for item in range(0, len(solution)-1):
@@ -57,50 +59,49 @@ def calculate_cost(solution, distances):
     cost = cost + cost_of_pair_of_cities
     return cost
     
-#def calculate_probability(initial_solution, candidate_solution):
-    
-    
-    
+
+# SA
 def SimulatedAnnealing(initial_solution, num_cities, distances, temperature):
     
     k = 0
     LIST_OF_SOLUTIONS = []
     best_solution = None
     best_solution_cost = None
-    
+    # Itera enquanto a solução não melhorar em k iterações
     while k < number_iterations_solution_is_not_improved:
-        
+        # Numero de soluções vizinhas locais a serem criadas
         for l in range(iterations_each_temperature):
-            
+            # Criação de uma solução candidata
             candidate_solution = create_new_solution(initial_solution, num_cities)
             
             #print(initial_solution)
             #print(candidate_solution)
             #return
+            
+            # Calculo dos custos de cada uma das soluções: candidata e inicial
             cost_X = calculate_cost(initial_solution, distances)
             cost_Y = calculate_cost(candidate_solution, distances)
             
             
-            #print(cost_X, cost_Y)
-            #return
             delta_C = cost_Y - cost_X
-            
+            # Verificar se a solução candidata é melhor que a solução inicial
             if delta_C < 0:
                 initial_solution = candidate_solution
             else:
+                # Caso seja pior ainda é possível aceita-la, considerando a temperatura e o quanto pior for a nova solução
                 variable_control = random.random()
                 
                 expo = (-1 * delta_C)/temperature
-                    
+                # Aceitação da solução candidata    
                 if variable_control <= pow(EULER, expo):
                     initial_solution = candidate_solution
-
+        # Atualização da temperatura
         temperature = temperature*alpha
         
         cost = calculate_cost(initial_solution, distances)
         print('Best Solution Cost --> ', cost, 'k: ', k, 'T: ', temperature)
         
-        
+        # A melhor solução para cada temperatura é armazenada
         if best_solution == None:
             best_solution = initial_solution
             best_solution_cost = calculate_cost(best_solution, distances)
@@ -117,7 +118,8 @@ def SimulatedAnnealing(initial_solution, num_cities, distances, temperature):
     record_solutions(solutions=LIST_OF_SOLUTIONS)
 
     return best_solution
-        
+
+# Gravação da solução em arquivo
 def record_solutions(solutions, code=code):
         with open('saidas/SA/' + code  + '_solution.csv', 'a') as file:
             file.write('Solution\n')
@@ -132,13 +134,13 @@ if __name__ == '__main__':
     alpha = 0.98
     iterations_each_temperature = 500
     number_iterations_solution_is_not_improved = 100
-
+    # Ler dados
     num_cities, distances = read_distances()
-    
+    # Criar solução inicial
     initial_solution = create_solution(num_cities)
-    
+    # 
     print(initial_solution)
-    
+    # Rodar SA
     final_solution = SimulatedAnnealing(initial_solution, num_cities, distances, temperature)
     final = time.time()
     
